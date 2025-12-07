fc8dc1dfe8939593be7da4c10d6fec3c8df8d937
Mostafa Nejati Hatamian <mostafa.nejati.hatamian@tum.de>
2025-12-07 18:24:03 +0100

    Squashed 'TUM Social App [Strangler Pattern]/' content from commit e4150c5
    git-subtree-dir: TUM Social App [Strangler Pattern]
    git-subtree-split: e4150c55a21bfbf6604e62a20fd491d36a0d423a

---
+++ login-microservice/src/test/java/eist24l03pb01/loginmicroservice/LoginMicroserviceApplicationTests.java
@@ -0,0 +1,11 @@
+package eist24l03pb01.loginmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginMicroserviceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
